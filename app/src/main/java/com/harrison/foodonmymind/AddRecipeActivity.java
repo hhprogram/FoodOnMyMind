d8776166b0f5829de2d85ee59bf7338696f3bebd
Harrison <li.harrison.h@gmail.com>
2017-07-06 16:12:38 -0700

    Fixed the add recipe layout to be able to scroll. Have prelim add ingredient button working

--- app/src/main/java/com/harrison/foodonmymind/AddRecipeActivity.java
+++ app/src/main/java/com/harrison/foodonmymind/AddRecipeActivity.java
@@ -5,6 +5,9 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 
 import java.util.ArrayList;
 
@@ -18,13 +21,20 @@ public class AddRecipeActivity extends AppCompatActivity {
 //    the code used so that when this activity gets a result back from an activity it launched with
 //    an intent - it can ensure that it is the correct request data
     private static final int SELECT_PICTURE = 1;
+//    used to hold all the photo paths that we want associated with this recipe
     private ArrayList<String> photoPaths;
+//    will be used to refer to when we want to add another ingredient
+    LinearLayout ingre_layout;
+//    will be used to refer to when we want to add another step
+    LinearLayout dir_layout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.add_recipe);
         photoPaths = new ArrayList<>();
+        ingre_layout = (LinearLayout) findViewById(R.id.ingredient_list);
+        dir_layout = (LinearLayout) findViewById(R.id.directions);
     }
 
 //    called once the Activity started by startActivityForResult() method returns data
@@ -33,7 +43,6 @@ public class AddRecipeActivity extends AppCompatActivity {
 //    the path to the actual image into the database so we can refer to it when loading up this
 //    recipe later
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
-
         if (resultCode == RESULT_OK) {
             if (requestCode == SELECT_PICTURE) {
                 Uri imageUri = data.getData();
@@ -44,11 +53,41 @@ public class AddRecipeActivity extends AppCompatActivity {
     }
 
     /**
-     * method called when the add ingredient button is clicked in the add recipe layout
+     * method called when the add ingredient button is clicked in the add recipe layout.
+     * First we create a new view RelativeLayout. And then we get this new view's layout parameters
+     * instance object. Then we set the width and height to constants that match what we have
+     * in the initial add_recipe layout
      * @param view
      */
     public void addIngredient(View view) {
-
+        RelativeLayout ingre_line = new RelativeLayout(this);
+        RelativeLayout.LayoutParams relParams = new RelativeLayout
+                .LayoutParams((int) getResources().getDimension(R.dimen.ingredient)
+                , RelativeLayout.LayoutParams.WRAP_CONTENT);
+        EditText new_ingre = new EditText(this);
+        RelativeLayout.LayoutParams ingreParams = new RelativeLayout
+                .LayoutParams((int) getResources().getDimension(R.dimen.quantity)
+                , RelativeLayout.LayoutParams.WRAP_CONTENT);
+        new_ingre.setHint(getString(R.string.ingre_hint));
+        new_ingre.setLayoutParams(ingreParams);
+        EditText new_quant = new EditText(this);
+        RelativeLayout.LayoutParams quantParams = new RelativeLayout
+                .LayoutParams((int) getResources().getDimension(R.dimen.ingredient)
+                , RelativeLayout.LayoutParams.WRAP_CONTENT);
+//        addRule is how i programatically align the editText view within this relativeLayout to be
+//        aligned to the right of its parent and to the right of the new_ingre view
+        quantParams.addRule(RelativeLayout.ALIGN_PARENT_RIGHT);
+        quantParams.addRule(RelativeLayout.RIGHT_OF, new_ingre.getId());
+        new_quant.setHint(getString(R.string.ingre_hint));
+        new_quant.setLayoutParams(quantParams);
+//        then adding these views to the parent RelativeLayout view so that android knows these 2
+//        should be children of the relativeLayout
+        ingre_line.addView(new_ingre);
+        ingre_line.addView(new_quant);
+        ingre_line.setLayoutParams(relParams);
+//        then at very end we add this new relative layout to the existing linear layout that holds
+//        the ingredient lists
+        ingre_layout.addView(ingre_line);
     }
 
     /**
